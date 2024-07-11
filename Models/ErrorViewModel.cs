c33d044f594c1fcd56ef5484fcf51c7850d95f7a
umesh yadav <iumeshpro@gmail.com>
2024-07-11 18:19:25 +0530

    https://github.com/umeshku

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mydotnetapptest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
